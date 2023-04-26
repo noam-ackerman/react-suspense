002279f885ef035450d2d14e985e01a06a451bb3
Noam Ackerman <developer.noam@gmail.com>
2023-04-26 18:22:56 +0300

    finishing exe 1,2,3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,36 +2,16 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
+import {PokemonDataView, fetchPokemon, PokemonErrorBoundary, PokemonInfoFallback} from '../pokemon'
+import {createResource} from '../utils'
 
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
-// 🐨 create a variable called "pokemon" (using let)
+let pokemomName = "pikachu";
+let pokemonResource = createResource(fetchPokemon(pokemomName))
 
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
-
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
-
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
+  const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -46,8 +26,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
+      <PokemonErrorBoundary>
+        <React.Suspense fallback={<PokemonInfoFallback name={pokemomName}/>}>
+            <PokemonInfo />
+        </React.Suspense>
+      </PokemonErrorBoundary>
       </div>
     </div>
   )
